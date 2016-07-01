72005b6280d3b0b900e586adfdaa1b96040f9fb0
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-07-01 14:35:00 +0930

    Added first_interval_simulation R script
    # Script for simulating concentrations for the first intervals
    # Everyone will receive 5 mg/kg doses for the first interval in all
    scenarios

---
+++ Project/infliximab_first_interval_simulation.R
@@ -0,0 +1,9 @@
+# Time-weighted Bayes project
+# Script for simulating concentrations for the first intervals
+# Everyone will receive 5 mg/kg doses for the first interval in all scenarios
+#-------------------------------------------------------------------------------
+# Source the other R scripts and execute
+	# infliximab_population.R
+	source(paste0(work.dir,"infliximab_population.R"))
+
+#-------------------------------------------------------------------------------
