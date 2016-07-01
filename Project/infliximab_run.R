fce4622ec7a853e4e314767f81491a0e12f56393
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-07-01 15:27:44 +0930

    Updating last R script in sequence
    The run file will source the last R script in sequence because it will
    then source all other files

--- Project/infliximab_run.R
+++ Project/infliximab_run.R
@@ -3,12 +3,12 @@
 #-------------------------------------------------------------------------------
 # Remove all current objects in the workspace
 	rm(list = ls(all = TRUE))
-	
+
 # Set working directory
 	work.dir <- "/Volumes/Prosecutor/PhD/InfliximabBayes/infliximab-bayes/Project/"
 	setwd(work.dir)
 
 #-------------------------------------------------------------------------------
 # Source the other R scripts and execute
-	# infliximab_population.R
-	source(paste0(work.dir,"infliximab_population.R"))
+	# infliximab_first_interval_simulation.R
+	source(paste0(work.dir,"infliximab_first_interval_simulation.R"))
