ceaa7a70d8a062be371d1a950bf6d035768c2432
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-07-02 14:05:01 +0930

    Renamed output data frame

--- Project/infliximab_first_interval_simulation.R
+++ Project/infliximab_first_interval_simulation.R
@@ -11,7 +11,7 @@
 
 # Create a data frame ready for mrgsolve simulation
 	# Function for creating a data frame ready for mrgsolve simulation
-		population.interval1.function <- function(input.data) {
+		interval1.label <- function(input.data) {
 			ID.number <- input.data$ID[1]	# Individual ID
 			SIM.number <- input.data$SIM[1]	# Individual simulation number
 			ALB <- input.data$ALB[input.data$TIME %in% TIME1]	# Individual albumin
@@ -20,7 +20,8 @@
 			ETA2 <- input.data$ETA2[input.data$TIME %in% TIME1]
 			ETA3 <- input.data$ETA3[input.data$TIME %in% TIME1]
 			ETA4 <- input.data$ETA4[input.data$TIME %in% TIME1]
-			input.conc.data1 <- data.frame(
+			ERRPRO <- input.data$ERRPRO[input.data$TIME %in% TIME1]
+			input.conc.data <- data.frame(
 				ID = ID.number,
 				SIM = SIM.number,
 				time = TIME1,	# Time points for simulation
@@ -30,32 +31,29 @@
 				ETA2,
 				ETA3,
 				ETA4,
+				ERRPRO,
 				amt = amt1*70,	# mg/kg dose
 				evid = 1,	# Dosing event
 				cmt = 1,	# Dose into the central compartment (compartment = 1)
 				rate = -2	# Infusion duration is specific in the model file
 			)
 			# Make the amt given in the last time-point == 0
-				input.conc.data1$amt[!c(input.conc.data1$time %in% TIME1i) | input.conc.data1$time == max(TIME1i)] <- 0
-				input.conc.data1$evid[!c(input.conc.data1$time %in% TIME1i) | input.conc.data1$time == max(TIME1i)] <- 0
-				input.conc.data1$rate[!c(input.conc.data1$time %in% TIME1i) | input.conc.data1$time == max(TIME1i)] <- 0
+				input.conc.data$amt[!c(input.conc.data$time %in% TIME1i) | input.conc.data$time == max(TIME1i)] <- 0
+				input.conc.data$evid[!c(input.conc.data$time %in% TIME1i) | input.conc.data$time == max(TIME1i)] <- 0
+				input.conc.data$rate[!c(input.conc.data$time %in% TIME1i) | input.conc.data$time == max(TIME1i)] <- 0
 			# Return input.conc.data
-				input.conc.data1
+				input.conc.data
 		}
 	# Create population data frame ready for mrgsolve simulation
-		input.conc.data1 <- ddply(pop.data, .(ID,SIM), population.interval1.function)
+		input.conc.data <- ddply(pop.data, .(ID,SIM), interval1.label)
 
 # Simulate concentration-time profiles for individuals in input.conc.data
-	per.simulation.function1 <- function(input.data) {
-		conc.data1 <- mod %>% data_set(input.data) %>% carry.out(SIM,amt) %>% mrgsim()
-		conc.data1 <- as.data.frame(conc.data1)
-	}
-	conc.data1 <- ddply(input.conc.data1, .(SIM), per.simulation.function1)
+	conc.data <- ddply(input.conc.data, .(SIM), conc.per.simulation)
 
 # ------------------------------------------------------------------------------
 # Test plot
 	plotobj1 <- NULL
-	plotobj1 <- ggplot(conc.data1)
+	plotobj1 <- ggplot(conc.data)
 	plotobj1 <- plotobj1 + stat_summary(aes(x = time,y = IPRE),geom = "line",fun.y = median,colour = "red")
 	plotobj1 <- plotobj1 + stat_summary(aes(x = time,y = IPRE),geom = "ribbon",fun.ymin = "CI95lo",fun.ymax = "CI95hi",fill = "red",alpha = 0.3)
 	plotobj1 <- plotobj1 + scale_y_log10("Infliximab Concentration (mg/L)\n",breaks = c(0.001,0.01,0.1,1,10,100,100),labels = c(0.001,0.01,0.1,1,10,100,100))
