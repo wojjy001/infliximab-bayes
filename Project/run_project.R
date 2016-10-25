2eecd5d56eb812c60667dcdc00ec24592364062e
Jessica Wojciechowski <jessica.wojciechowski@mymail.unisa.edu.au>
2016-10-25 12:00:25 +1030

    Re-run specific seeds from output folder

--- Project/run_project.R
+++ Project/run_project.R
@@ -30,23 +30,24 @@
 
 # ------------------------------------------------------------------------------
 # Read in seeds from previous output
-	project.dir <- "E:/Wojciechowski/Moved-Infliximab-Output/"
-  file.list <- list.files(path = project.dir,pattern = "SUCCESS") # List of successful
-	library(stringr)
-	library(plyr)
-	library(dplyr)
-	num.list <- str_extract_all(file.list,pattern = "[0-9]")	# Find all the numbers in the filename
-	numlist2 <- llply(num.list,function(num.list) paste(num.list,collapse = ""))	# Paste their numbers back together
-	num.unlist <- unlist(numlist2)	# Unlist them
-	num.unlist.split <- str_split(num.unlist,pattern = "[9]",n = 2)	# Where the first 9 occurs, split the string into 2
-	file.data <- structure(data.frame(matrix(unlist(num.unlist.split),length(num.unlist),2,T)))
-	names(file.data)[c(1,2)] <- c("nsim","seed")
-	file.data$nsim <- as.numeric(levels(file.data$nsim))[file.data$nsim]
-	file.data$seed <- as.numeric(levels(file.data$seed)[file.data$seed])
-
-	for (i in 1:nrow(file.data)) {
-		nsim <- file.data$nsim[i]
-		seed <- file.data$seed[i]
+	# project.dir <- "E:/Wojciechowski/Moved-Infliximab-Output/"
+  # file.list <- list.files(path = project.dir,pattern = "SUCCESS") # List of successful
+	# library(stringr)
+	# library(plyr)
+	# library(dplyr)
+	# num.list <- str_extract_all(file.list,pattern = "[0-9]")	# Find all the numbers in the filename
+	# numlist2 <- llply(num.list,function(num.list) paste(num.list,collapse = ""))	# Paste their numbers back together
+	# num.unlist <- unlist(numlist2)	# Unlist them
+	# num.unlist.split <- str_split(num.unlist,pattern = "[9]",n = 2)	# Where the first 9 occurs, split the string into 2
+	# file.data <- structure(data.frame(matrix(unlist(num.unlist.split),length(num.unlist),2,T)))
+	# names(file.data)[c(1,2)] <- c("nsim","seed")
+	# file.data$nsim <- as.numeric(levels(file.data$nsim))[file.data$nsim]
+	# file.data$seed <- as.numeric(levels(file.data$seed)[file.data$seed])
+	#
+	# for (i in c(4:5,44,55)) {
+	# 	nsim <- file.data$nsim[i]
+	# 	seed <- file.data$seed[i]
+		# print(paste0("seed ",seed," nsim ",nsim))
 	# Run "single-run" simulation files
 	# First standard interval simulation (initial dose is 5 mg/kg)
 		suppressPackageStartupMessages(	# Suppress package loading messages
@@ -54,15 +55,15 @@
 				source(paste0(work.dir,"first_interval1.R"))
 			)
 		)
-	# # First standard interval simulation (initial dose is 10 mg/kg)
-	# 	suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"first_interval2.R"))))
+	# First standard interval simulation (initial dose is 10 mg/kg)
+		suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"first_interval2.R"))))
 	# Label simulation
 		suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"label.R"))))
-	# # Clinical simulation where doses are adjusted based on trough concentrations (DV)
-	# 	suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"clinical_TDM.R"))))
-	# # Clinical simulation
-	# 	suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"clinical.R"))))
-	# # Run the various Bayes estimation scenarios sequentially
-	# # Scenarios with no time-weighting
-	# 	suppressWarnings(source(paste0(work.dir,"bayes.R")))
-	}
+	# Clinical simulation where doses are adjusted based on trough concentrations (DV)
+		suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"clinical_TDM.R"))))
+	# Clinical simulation
+		suppressPackageStartupMessages(suppressWarnings(source(paste0(work.dir,"clinical.R"))))
+	# Run the various Bayes estimation scenarios sequentially
+	# Scenarios with no time-weighting
+		suppressWarnings(source(paste0(work.dir,"bayes.R")))
+	# }
